8c01469fe5263190351c38768997e109b50f3829
Nelson Cardenas <nelsonjc.01@gmail.com>
2016-03-30 17:25:54 -0600

    change heroku service links

--- app/scripts/services/authService.js
+++ app/scripts/services/authService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('AuthService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'https://examen2backend.herokuapp.com/';
+		var baseUrl = 'https://nelson-cardenas.herokuapp.com/';
 		return {
 				Logout: function(){
 					return $http.get(baseUrl + "v1/logout");
